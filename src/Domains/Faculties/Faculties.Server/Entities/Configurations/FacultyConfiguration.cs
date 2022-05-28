9c0e26b9dcdf723b6fb07d1925cf302cbd5ccae7
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-05-29 01:22:57 +0200

    map faculty to faculties table

--- src/Domains/Faculties/Faculties.Server/Entities/Configurations/FacultyConfiguration.cs
+++ src/Domains/Faculties/Faculties.Server/Entities/Configurations/FacultyConfiguration.cs
@@ -1,6 +1,13 @@
 ﻿namespace Faculties.Server.Entities.Configurations;
 
+using Microsoft.EntityFrameworkCore;
+
 public class FacultyConfiguration : BaseSettingConfiguration<Faculty>
 {
-    public override void Configure(EntityTypeBuilder<Faculty> builder) => base.Configure(builder);
+    public override void Configure(EntityTypeBuilder<Faculty> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("Faculties");
+    }
 }
