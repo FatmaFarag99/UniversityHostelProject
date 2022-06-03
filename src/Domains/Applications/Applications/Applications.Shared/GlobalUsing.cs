6f1014da38740ba9f24d7113a1bd5b041475044c
Hazem <hazemahmedamr.zero@gmail.com>
2022-06-03 23:54:20 +0200

    Move BasicInformation and Documents Shared to Applications.Shared

--- src/Domains/Applications/Applications/Applications.Shared/GlobalUsing.cs
+++ src/Domains/Applications/Applications/Applications.Shared/GlobalUsing.cs
@@ -1,2 +1,7 @@
 ﻿global using CommonLibrary;
 global using Applications.Shared.ViewModels;
+global using Faculties.Shared.ViewModels;
+global using Residences.Shared.ViewModels;
+global using System;
+global using FluentValidation;
+global using Documents.Shared.ViewModels;
--- src/Domains/Applications/Applications/Applications.Shared/ViewModels/ApplicationViewModel.cs
+++ src/Domains/Applications/Applications/Applications.Shared/ViewModels/ApplicationViewModel.cs
@@ -1,10 +1,21 @@
 ﻿namespace Applications.Shared.ViewModels;
 
 using Applications.Shared.Enums;
+using BasicInformations.Shared;
+using Documents.Shared.ViewModels;
 
 public class ApplicationViewModel : BaseViewModel
 {
     public ApplicationStatus Status { get; set; }
     public string UserId { get; set; }
     public bool IsComplete { get; set; }
+
+    public Guid PaymentId { get; set; }
+    
+    public Guid BasicInformationId { get; set; }
+    public BasicInformationViewModel BasicInformation { get; set; }
+
+    public Guid DocumentsId { get; set; }
+    public DocumentViewModel Documents { get; set; }
+
 }
