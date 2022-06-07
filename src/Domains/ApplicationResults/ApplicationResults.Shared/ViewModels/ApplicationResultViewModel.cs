47b8a5534961ce028105a1f4d70c82250784ddb1
FatmaFarag <fatmafarag17799@gmail.com>
2022-06-07 00:19:37 -0700

    Update

---
+++ src/Domains/ApplicationResults/ApplicationResults.Shared/ViewModels/ApplicationResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ApplicationResults.Shared
+{
+    using Applications.Shared.Enums;
+    using CommonLibrary.Server;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ApplicationResultViewModel : BaseEntity
+    {
+        public ApplicationStatus ApplicationStatus { get; set; }
+    }
+}
