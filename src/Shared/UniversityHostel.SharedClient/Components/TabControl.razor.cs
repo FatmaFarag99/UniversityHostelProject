10e82092d4f385d9457d49bf709a51cab186c427
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-06-16 01:54:05 +0200

    add tab control component

---
+++ src/Shared/UniversityHostel.SharedClient/Components/TabControl.razor.cs
@@ -0,0 +1,36 @@
+﻿namespace UniversityHostel.SharedClient.Components
+{
+    using SmartUI.Forms;
+    using System.Collections.Generic;
+
+    public partial class TabControl : BaseComponent
+    {
+
+        private List<TabPage> tapPages = new List<TabPage>();
+        public TabPage ActiveTab { get; set; }
+
+        public void AddTapPage(TabPage tabPage)
+        {
+            tapPages.Add(tabPage);
+            if (tapPages.Count == 1)
+                ActiveTab = tabPage;
+
+            StateHasChanged();
+        }
+
+        private string GetNavLinkCssClass(TabPage tabPage)
+        {
+            string cssClass = ActiveTab == tabPage ? "active " : "";
+            cssClass += tabPage.Disabled ? "disabled " : "";
+
+            return cssClass;
+        }
+
+        private void ActivePage(TabPage tabPage) => ActiveTab = tabPage;
+
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public string CssClass { get; set; }
+        [Parameter] public string Id { get; set; }
+    }
+}
---
+++ src/Shared/UniversityHostel.SharedClient/Components/TabPage.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace UniversityHostel.SharedClient.Components
+{
+    using SmartUI.Forms;
+
+    public partial class TabPage : BaseComponent
+    {
+        [CascadingParameter(Name = "ParentTapControl")]
+        public TabControl ParentTapControl { get; set; }
+        [Parameter] public string Title { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public bool Disabled { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (ParentTapControl is null)
+                throw new ArgumentNullException(nameof(ParentTapControl), "TabPage must exist within a TabControl");
+
+            ParentTapControl.AddTapPage(this);
+
+            base.OnInitialized();
+        }
+    }
+}
