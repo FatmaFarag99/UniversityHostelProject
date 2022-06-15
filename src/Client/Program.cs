3ac9ea9f6bb285cf8525c678c3af61461d946517
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-06-15 06:01:52 +0200

    Use Services.AddSmartGrid() in program to support SmartUI.Grid library

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -2,6 +2,7 @@ using Blazored.Toast;
 using UniversityHostel.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SmartUI.Grid.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#page-top");
@@ -11,5 +12,6 @@ builder.Services.ConfigureFluentValidationServices();
 builder.Services.ConfigureAuthentication(builder.HostEnvironment);
 builder.Services.ConfigureHttpServices();
 builder.Services.AddBlazoredToast();
+builder.Services.AddSmartGrid();
 
 await builder.Build().RunAsync();
