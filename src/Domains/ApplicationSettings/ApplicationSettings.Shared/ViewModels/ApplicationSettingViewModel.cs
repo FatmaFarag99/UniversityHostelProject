6ccca31e4cdd2e2a1027519a9602bcbe3c8914cb
FatmaFarag <fatmafarag17799@gmail.com>
2022-06-16 00:23:37 -0700

    Update

--- src/Domains/ApplicationSettings/ApplicationSettings.Shared/ViewModels/ApplicationSettingViewModel.cs
+++ src/Domains/ApplicationSettings/ApplicationSettings.Shared/ViewModels/ApplicationSettingViewModel.cs
@@ -3,5 +3,5 @@ namespace ApplicationSettings.Shared.ViewModels;
 public class ApplicationSettingViewModel : BaseViewModel
 {
     public DateTime PhaseEndTime { get; set; } = DateTime.Now;
-    public bool IsRegistrationEnabled { get; set; }
+    public bool IsRegistrationEnabled { get; set; } = true;
 }
