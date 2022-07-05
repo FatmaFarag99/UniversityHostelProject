ace29d5386d3bbecd9014d43f6a3b0d28674f0eb
Hazem <hazemahmedamr.zero@gmail.com>
2022-07-05 23:06:09 +0200

    Fix dashboard slider

--- src/Client/wwwroot/main.js
+++ src/Client/wwwroot/main.js
@@ -1,53 +1,57 @@
-var sliderImages = Array.from(document.querySelectorAll(".jell img")),
-slideCount = sliderImages.length,
-currentSlide = 1,
+function startXX() {
+    setTimeout(function () {
+        var sliderImages = Array.from(document.querySelectorAll(".jell img")),
+            slideCount = sliderImages.length,
+            currentSlide = 1,
 
-nextbutton = document.getElementById('next'),
-prevbutton = document.getElementById('prev');
+            nextbutton = document.getElementById('next'),
+            prevbutton = document.getElementById('prev');
 
-nextbutton.onclick = nextSlide;
-prevbutton.onclick = prevSlide;
+        nextbutton.onclick = nextSlide;
+        prevbutton.onclick = prevSlide;
 
-theCheacker();
-
-function nextSlide(){
-    if(nextbutton.classList.contains('disabled')){
-        return false;
-    }
-    else{
-        currentSlide++;
         theCheacker();
-    }
-}
-function prevSlide(){
-    if(prevbutton.classList.contains('disabled')){
-        return false;
-    }
-    else{
-        currentSlide--;
-        theCheacker();
-    }
-}
-function theCheacker(){
-    removeAllActive();
 
-    sliderImages[currentSlide - 1].classList.add('active');
+        function nextSlide() {
+            if (nextbutton.classList.contains('disabled')) {
+                return false;
+            }
+            else {
+                currentSlide++;
+                theCheacker();
+            }
+        }
+        function prevSlide() {
+            if (prevbutton.classList.contains('disabled')) {
+                return false;
+            }
+            else {
+                currentSlide--;
+                theCheacker();
+            }
+        }
+        function theCheacker() {
+            removeAllActive();
 
-    if(currentSlide == 1){
-        prevbutton.classList.add('disabled');
-    }
-    else{
-        prevbutton.classList.remove('disabled');
-    }
-    if(currentSlide == slideCount){
-        nextbutton.classList.add('disabled');
-    }
-    else{
-        nextbutton.classList.remove('disabled');
-    }
-}
-function removeAllActive(){
-    sliderImages.forEach(function(img){
-        img.classList.remove('active');
-    });
+            sliderImages[currentSlide - 1].classList.add('active');
+
+            if (currentSlide == 1) {
+                prevbutton.classList.add('disabled');
+            }
+            else {
+                prevbutton.classList.remove('disabled');
+            }
+            if (currentSlide == slideCount) {
+                nextbutton.classList.add('disabled');
+            }
+            else {
+                nextbutton.classList.remove('disabled');
+            }
+        }
+        function removeAllActive() {
+            sliderImages.forEach(function (img) {
+                img.classList.remove('active');
+            });
+        }
+    }, 500);
 }
